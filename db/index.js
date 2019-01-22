37599ac6691c0c1ebfb9b841db824d9a3117800b
sfdcadmbot <45421176+sfdcadmbot@users.noreply.github.com>
2019-01-23 00:35:32 +0530

    Update index.js

--- db/index.js
+++ db/index.js
@@ -13,11 +13,11 @@ module.exports = {
     const userInsertStatement = 'INSERT INTO public."user"("firstname", "lastname","primaryemail") VALUES($1, $2, $3) RETURNING *'
     const userInsertValues = [params.firstname,params.lastname,params.Email]
     const  userrows  = await pool.query(userInsertStatement,userInsertValues )
-    //console.log(JSON.stringify(userrows.rows[0]))
+    console.log('value for user table:'+JSON.stringify(userrows.rows[0]))
     const idpInsertStatement = 'INSERT INTO public."IdentityProviders"("instanceUrl", "access_token","refresh_token","Email","userId","salesforceId","organizationId") VALUES($1, $2, $3, $4, $5, $6, $7) RETURNING *'
     const idpInsertValues = [params.instanceUrl,params.access_token,params.refresh_token,params.Email,userrows.rows[0].Id,params.salesforceId,params.organizationId]
     const idprows = await pool.query(idpInsertStatement,idpInsertValues )
-    //console.log(JSON.stringify(idprows.rows[0]))
+    console.log('value for IP table:'+JSON.stringify(idprows.rows[0]))
     return userrows.rows[0].Id 
   }
 }
