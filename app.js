c19373bb0c99ed1ac99b073211f9430e207768ac
sfdcadmbot <45421176+sfdcadmbot@users.noreply.github.com>
2019-01-24 19:26:23 +0530

    Update app.js

--- app.js
+++ app.js
@@ -533,8 +533,10 @@ app.intent('Default Welcome Intent', (conv) => {
 		console.log('The value here access token-->'+resp[0].access_token);
 		console.log('The value here goog id-->'+resp[0].googleid);
 
-		if(resp[0].googleid != Undefined)
+		if(resp[0].googleid!='')
+		{
 			console.log('Instance Url'+ resp[0].instanceUrl);
+		}
 		else
 		{
 		var recid=db.updateUser({
