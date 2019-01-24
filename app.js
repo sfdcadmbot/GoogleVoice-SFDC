d097eaeaf02325f7dc4771ffccfcd84080713aaa
sfdcadmbot <45421176+sfdcadmbot@users.noreply.github.com>
2019-01-24 16:55:51 +0530

    Update app.js

--- app.js
+++ app.js
@@ -499,7 +499,7 @@ app.intent('Default Welcome Intent', (conv) => {
 	console.log('conv.user',conv.user);
 	var test=parseInt(3);
 	const result = db.query('SELECT * FROM public."IdentityProviders" WHERE "id" = $1',
-          test)
+          [test])
 	 
 	  
 	  //const result = db.query("SELECT Id from IdentityProviders where Id = 3");
