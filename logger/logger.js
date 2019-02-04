ec86bf4b9903d7855301d67b5f777eab4bb182cf
sfdcadmbot <45421176+sfdcadmbot@users.noreply.github.com>
2019-02-05 00:32:12 +0530

    Update logger.js

--- logger/logger.js
+++ logger/logger.js
@@ -1,6 +1,6 @@
 var fileSystem=require('fs');
 var path=require('path');
-var folderName='https://github.com/sfdcadmbot/GoogleVoice-SFDC/tree/master/logs';
+var folderName='https://github.com//sfdcadmbot//GoogleVoice-SFDC//tree//master//logs';
 var fileName=new Date().getDate().toString()+'-'+(new Date().getMonth()+1).toString()+'-log.txt';
 //var folderName = require(path.join(__dirname + '/logs'));
 exports.log=(data)=>{
