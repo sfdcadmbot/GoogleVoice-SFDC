7d556630cbadf0389bfde88a07fce38b605c01ef
sfdcadmbot <45421176+sfdcadmbot@users.noreply.github.com>
2019-02-05 17:32:17 +0530

    Update logger.js

--- logger/logger.js
+++ logger/logger.js
@@ -8,6 +8,6 @@ Logger.log = function(msg) {
   console.log('Req came here'+msg);
   var message = new Date().toISOString() + " : " + msg + "\n";
   console.log('message came here'+message);
-  logupdated.writeFileSync(message);
+  fileSystem.writeFileSync(message);
 };
 
