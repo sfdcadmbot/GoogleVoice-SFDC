96e08e6c62c11af7bc07ad2988299d1fc871cd32
sfdcadmbot <45421176+sfdcadmbot@users.noreply.github.com>
2019-02-05 14:49:02 +0530

    Update logger.js

--- logger/logger.js
+++ logger/logger.js
@@ -2,10 +2,15 @@ var fileSystem=require('fs');
 
 var Logger = exports.Logger = {};
 
-var logupdated = fileSystem.createWriteStream('logs/log.txt',{flags:'a'});
+var logupdated = fileSystem.createWriteStream('/logs/log.txt',{flags:'a'});
 
 Logger.log = function(msg) {
   console.log('Req came here');
   var message = new Date().toISOString() + " : " + msg + "\n";
-  logupdated.write(message);
-};
+  logupdated.write(message,function(err) {
+    if(err) {
+        console.log('Err here-->'+err);
+    }
+
+    console.log("The file was saved!");
+});
