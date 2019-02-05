72c8aaa0a7bfd8dfb35010e8547fd9ef02ede153
sfdcadmbot <45421176+sfdcadmbot@users.noreply.github.com>
2019-02-05 17:52:24 +0530

    Update app.js

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const db = require('./db');
 const config = require('./config/config');
 const pg = require('pg');
 const pool = new pg.Pool(config.db);
-var logger=require('./logger/logger').Logger;
+//var logger=require('./logger/logger').Logger;
 var cookieParser = require('cookie-parser')
 var strname = ''; 
 
@@ -1241,7 +1241,7 @@ server.post('/fulfillment',app);
 
 server.listen(port, function () {
 	console.log('port',port);
-	logger.log(port);
+	//logger.log(port);
     console.log("Server is up and running...");
 });
 
