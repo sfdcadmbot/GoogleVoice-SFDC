86779c9e1efcbc2237349e4f189af248f0cc90ad
sfdcadmbot <45421176+sfdcadmbot@users.noreply.github.com>
2019-02-05 17:42:15 +0530

    Update logger.js

--- logger/logger.js
+++ logger/logger.js
@@ -2,11 +2,12 @@ var fileSystem=require('fs');
 
 var Logger = exports.Logger = {};
 
-var logupdated = fileSystem.createWriteStream('logtesting.txt');
+//var logupdated = fileSystem.createWriteStream('logtesting.txt');
 
 Logger.log = function(msg) {
   console.log('Req came here'+msg);
   var message = new Date().toISOString() + " : " + msg + "\n";
   console.log('message came here'+message);
-  fileSystem.writeFileSync(logupdated,message);
+  //fileSystem.writeFileSync(logupdated,message);
+  fileSystem.writeFileSync('logtesting.txt', message, { mode: 0o755 });
 };
