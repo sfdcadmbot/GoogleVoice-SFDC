0c8649c084e00f13e9caaebd3b39e09157796554
sfdcadmbot <45421176+sfdcadmbot@users.noreply.github.com>
2019-02-07 17:32:24 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1561,8 +1561,8 @@ app.intent('Check Permission Set Assignment', (conv,params) => {
                     conv.ask(new SimpleResponse({speech:"Error while fetching Namespace",text:"Error while fetching namespace"}));
 				}
 				else{
-					var restURL = "/learnlite/checkPermSetAssignment?permSetName=" + params.permSetName + "&userName=" + params.userName;
-                    //restURL = (result.records[0].NamespacePrefix != null) ? ("/" + result.records[0].NamespacePrefix + restURL) : (restURL);
+					var restURL = "/checkPermSetAssignment?permSetName=" + params.permSetName + "&userName=" + params.userName;
+                    restURL = (result.records[0].NamespacePrefix != null) ? ("/" + result.records[0].NamespacePrefix + restURL) : (restURL);
 					response.apex.get(restURL, options, function(err, resp) {
 						if (err) {
 							conv.ask(new SimpleResponse({
