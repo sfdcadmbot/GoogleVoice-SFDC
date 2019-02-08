f178f5202ab79e2f8118c55d55bac619f691055a
sfdcadmbot <45421176+sfdcadmbot@users.noreply.github.com>
2019-02-08 23:05:15 +0530

    Update logger.js

--- logger/logger.js
+++ logger/logger.js
@@ -2,7 +2,7 @@ var fs=require('fs');
 
 var logger=function(msg){
     console.log(`Message came ${msg}`);
-    fs.writeFileSync('../logs/logs.txt',`${new Date().toISOString()} :  ${msg} \n`);
+    fs.writeFileSync('../logs/logger.txt',`${new Date().toISOString()} :  ${msg} \n`);
     console.log('done');
 }
 module.exports.logger=logger;
