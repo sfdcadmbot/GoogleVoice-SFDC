88db4cebe7b87e25d6468fc37d66a3fc3c6bbb23
sfdcadmbot <45421176+sfdcadmbot@users.noreply.github.com>
2019-02-08 11:28:00 +0530

    Update logger.js

--- logger/logger.js
+++ logger/logger.js
@@ -1,4 +1,4 @@
-var fileSystem=require('fs');
+/*var fileSystem=require('fs');
 
 var Logger = exports.Logger = {};
 
@@ -10,4 +10,42 @@ Logger.log = function(msg) {
   console.log('message came here'+message);
   //fileSystem.writeFileSync(logupdated,message);
   fileSystem.writeFileSync('./logtesting.txt', message);
-};
+};*/
+
+
+/**
+ * Configurations of logger.
+ */
+const winston = require('winston');
+const winstonRotator = require('winston-daily-rotate-file');
+
+const consoleConfig = [
+  new winston.transports.Console({
+    'colorize': true
+  })
+];
+
+const createLogger = new winston.Logger({
+  'transports': consoleConfig
+});
+
+const successLogger = createLogger;
+successLogger.add(winstonRotator, {
+  'name': 'access-file',
+  'level': 'info',
+  'filename': './logs/access.log',
+  'json': false,
+  'datePattern': 'yyyy-MM-dd-',
+  'prepend': true
+});
+
+const errorLogger = createLogger;
+errorLogger.add(winstonRotator, {
+  'name': 'error-file',
+  'level': 'error',
+  'filename': './logs/error.log',
+  'json': false,
+  'datePattern': 'yyyy-MM-dd-',
+  'prepend': true
+});
+
