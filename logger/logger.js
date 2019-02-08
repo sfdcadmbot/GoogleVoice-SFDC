898aed6066226e809c976a331668d8092d6314ef
sfdcadmbot <45421176+sfdcadmbot@users.noreply.github.com>
2019-02-08 22:59:10 +0530

    Update logger.js

--- logger/logger.js
+++ logger/logger.js
@@ -1,52 +1,8 @@
-var appRoot = require('app-root-path');
-//var path = require('path');
-var path= __dirname+'/';
-console.log('appRoot'+path);
+var fs=require('fs');
 
-var winston = require('winston');
-
-var options = {
-  file: {
-    level: 'info',
-    filename: path+'app.log',
-    handleExceptions: true,
-    json: true,
-    maxsize: 5242880, // 5MB
-    maxFiles: 100,
-    colorize: true,
-  },
-  errorFile: {
-    level: 'error',
-    filename: path+'error.log',
-    handleExceptions: true,
-    json: true,
-    maxsize: 5242880, // 5MB
-    maxFiles: 100,
-    colorize: true,
-  },
-  console: {
-    level: 'debug',
-    handleExceptions: true,
-    json: false,
-    colorize: true,
-  },
-};
-
-
-// your centralized logger object
-let logger = winston.createLogger({
-  transports: [
-    new (winston.transports.Console)(options.console),
-    new (winston.transports.File)(options.errorFile),
-    new (winston.transports.File)(options.file)
-  ],
-  exitOnError: false, // do not exit on handled exceptions
-});
-
-module.exports = logger;
-module.exports.stream = {
-    write: function(message, encoding){
-        logger.info(message);
-    }
-};
-
+var logger=function(msg){
+    console.log(`Message came ${msg}`);
+    fs.writeFileSync('../log/logger.txt',`${new Date().toISOString()} :  ${msg} \n`);
+    console.log('done');
+}
+module.exports.logger=logger;
