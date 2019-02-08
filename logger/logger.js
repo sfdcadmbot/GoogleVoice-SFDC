823afe739959aeea2382520eccd9899cae2fb58c
sfdcadmbot <45421176+sfdcadmbot@users.noreply.github.com>
2019-02-08 23:21:58 +0530

    Update logger.js

--- logger/logger.js
+++ logger/logger.js
@@ -2,6 +2,7 @@ var fs=require('fs');
 
 var logger=function(msg){
     console.log(`Message came ${msg}`);
+    fs.readdir('../',(err,files)=>{console.log('Directory Info-->',files);});
     fs.writeFile('logger.txt',`${new Date().toISOString()} :  ${msg} \n`,(err)=>{
             if(err){
                 console.log('Error happened at logging-->',err);
