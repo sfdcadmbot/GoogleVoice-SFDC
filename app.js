2ac26553b9b4d738b163188c15285025e01d193e
sfdcadmbot <45421176+sfdcadmbot@users.noreply.github.com>
2019-03-04 23:50:08 +0530

    Update app.js

--- app.js
+++ app.js
@@ -49,6 +49,7 @@ var oauth2 = new jsforce.OAuth2(config.oauth);
 // Serve static assets
 server.use('/', function(req, res, next) {
     console.log('Header:'+JSON.stringify(req.headers));
+	console.log('Body:'+JSON.stringify(req.body));
     let token = req.headers['x-access-token'] || req.headers['authorization']; // Express headers are auto converted to lowercase
 	 //console.log('req.headers['x-access-token']'+req.headers['x-access-token']);
          //console.log('req.headers['authorization']:'+req.headers['authorization']);
