a27a10044c861a6bc435441785a63ded0046b09d
sfdcadmbot <45421176+sfdcadmbot@users.noreply.github.com>
2019-03-21 14:49:49 +0530

    Update app.js

--- app.js
+++ app.js
@@ -516,7 +516,7 @@ app.intent('Search for custom settings', (conv,params) => {
                     conv.ask(new SimpleResponse({speech:"Error while fetching Namespace",text:"Error while fetching namespace"}));
 				}
 				else{
-					var restURL = "/getCustomSettings?custSettName=" + params.custSettName + "&namespace" + result.records[0].NamespacePrefix;
+					var restURL = "/getCustomSettings?custSettName=" + params.custSettName + "&namespace=" + result.records[0].NamespacePrefix;
                     restURL = (result.records[0].NamespacePrefix != null) ? ("/" + result.records[0].NamespacePrefix + restURL) : (restURL);
 					response.apex.get(restURL, options, function(err, resp) {
 						if (err) {
